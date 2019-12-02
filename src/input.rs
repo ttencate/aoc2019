a639efb864a53db2fedbb42e4cc70c948e1b998e
Thomas ten Cate <ttencate@gmail.com>
2019-12-02 09:54:30 +0100

    Ask for session cookie if not found

--- src/input.rs
+++ src/input.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 use std::fs;
 use std::io;
+use std::io::BufRead;
 
 pub fn get_input(year: u32, day: u32) -> String {
     let input_file_name = input_file_name(year, day);
@@ -19,8 +20,16 @@ fn input_file_name(_year: u32, day: u32) -> String {
 }
 
 fn load_session_cookie() -> Result<String, io::Error> {
-    fs::read_to_string(".session_cookie")
+    let cookie_file_name = ".session_cookie";
+    fs::read_to_string(cookie_file_name)
         .map(|s| s.trim().to_string())
+        .or_else(|_err| -> Result<String, io::Error> {
+            println!("No session cookie found. Please log in to https://adventofcode.com/ in your browser, open the browser console, copy the value of the 'session' cookie, and paste it here:");
+            let mut line = String::new();
+            io::stdin().lock().read_line(&mut line)?;
+            fs::write(&cookie_file_name, &line)?;
+            Ok(line.trim().to_string())
+        })
 }
 
 fn fetch_input(year: u32, day: u32) -> Result<String, Box<dyn Error>> {

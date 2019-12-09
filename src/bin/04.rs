1a524566bd7243956c0773587f407d781185dcfd
Thomas ten Cate <ttencate@gmail.com>
2019-12-09 21:04:27 +0100

    Optimize 04 by only iterating over numbers with non-decreasing digits

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -27,37 +27,84 @@ fn test_has_two_adjacent_digits() {
     assert_eq!(has_two_adjacent_digits(b"111122"), true);
 }
 
-fn has_decreasing_digits(s: &[u8]) -> bool {
-    s.iter().zip(s.iter().skip(1)).any(|(a, b)| a > b)
+fn make_increasing(mut cur: Vec<u8>) -> Vec<u8> {
+    for i in 1..cur.len() {
+        if cur[i] < cur[i - 1] {
+            cur[i] = cur[i - 1];
+        }
+    }
+    cur
 }
 
 #[test]
-fn test_has_decreasing_digits() {
-    assert_eq!(has_decreasing_digits(b"111111"), false);
-    assert_eq!(has_decreasing_digits(b"223450"), true);
-    assert_eq!(has_decreasing_digits(b"123789"), false);
+fn test_make_increasing() {
+    assert_eq!(make_increasing("123456".as_bytes().to_vec()), "123456".as_bytes().to_vec());
+    assert_eq!(make_increasing("127456".as_bytes().to_vec()), "127777".as_bytes().to_vec());
+    assert_eq!(make_increasing("923256".as_bytes().to_vec()), "999999".as_bytes().to_vec());
+}
+
+fn increment(mut cur: Vec<u8>) -> Option<Vec<u8>> {
+    for i in (0..cur.len()).rev() {
+        if cur[i] < b'9' {
+            cur[i] += 1;
+            for j in i + 1..cur.len() {
+                cur[j] = cur[i];
+            }
+            return Some(cur);
+        }
+    }
+    None
+}
+
+#[test]
+fn test_increment() {
+    assert_eq!(increment("123456".as_bytes().to_vec()), Some("123457".as_bytes().to_vec()));
+    assert_eq!(increment("123459".as_bytes().to_vec()), Some("123466".as_bytes().to_vec()));
+    assert_eq!(increment("127777".as_bytes().to_vec()), Some("127778".as_bytes().to_vec()));
+    assert_eq!(increment("999999".as_bytes().to_vec()), None);
+}
+
+fn parse_input(input: &str) -> (Vec<u8>, Vec<u8>) {
+    let from_to = input.trim().split('-').collect::<Vec<_>>();
+    let from = from_to[0].as_bytes().to_vec();
+    let to = from_to[1].as_bytes().to_vec();
+    assert_eq!(from.len(), to.len());
+    (from, to)
 }
 
 fn part1(input: &str) -> usize {
-    let from_to = input.trim().split('-').map(|s| s.parse::<u64>().unwrap()).collect::<Vec<_>>();
-    (from_to[0]..from_to[1])
-        .filter(|n| {
-            let s = n.to_string();
-            s.len() == 6 && has_adjacent_digits(s.as_bytes()) && !has_decreasing_digits(s.as_bytes())
-        })
-        .count()
+    let (from, to) = parse_input(input);
+    let mut cur = make_increasing(from.to_vec());
+
+    let mut count = 0;
+    while cur < to {
+        if has_adjacent_digits(&cur) {
+            count += 1;
+        }
+        cur = increment(cur).unwrap();
+    }
+    count
 }
 
 fn part2(input: &str) -> usize {
-    let from_to = input.trim().split('-').map(|s| s.parse::<u64>().unwrap()).collect::<Vec<_>>();
-    (from_to[0]..from_to[1])
-        .filter(|n| {
-            let s = n.to_string();
-            s.len() == 6 && has_two_adjacent_digits(s.as_bytes()) && !has_decreasing_digits(s.as_bytes())
-        })
-        .count()
+    let (from, to) = parse_input(input);
+    let mut cur = make_increasing(from.to_vec());
+
+    let mut count = 0;
+    while cur < to {
+        if has_two_adjacent_digits(&cur) {
+            count += 1;
+        }
+        cur = increment(cur).unwrap();
+    }
+    count
 }
 
 fn main() {
     aoc::main(part1, part2);
+}
+
+#[test]
+fn test_answers() {
+    aoc::test(part1, 979, part2, 635);
 }

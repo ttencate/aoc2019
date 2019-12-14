b358375e55f7a991f452ad5794b8239c308508a1
Thomas ten Cate <ttencate@gmail.com>
2019-12-14 16:15:27 +0100

    Use a stricter lower bound for the amount of fuel produced

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -166,11 +166,11 @@ fn test_part1() {
 }
 
 fn max_fuel_from_ore(input_ore: usize, ordered_reactions: &Vec<Reaction>) -> usize {
-    let mut upper_fuel = 1;
+    let mut lower_fuel = input_ore / ore_needed_for_fuel(1, ordered_reactions);
+    let mut upper_fuel = lower_fuel * 2;
     while ore_needed_for_fuel(upper_fuel, ordered_reactions) <= input_ore {
         upper_fuel *= 2;
     }
-    let mut lower_fuel = 0;
     while lower_fuel + 1 < upper_fuel {
         let mid_fuel = (lower_fuel + upper_fuel) / 2;
         if ore_needed_for_fuel(mid_fuel, ordered_reactions) <= input_ore {

96ec354e204488f4c1a59fb07b8335b00ebabd36
Thomas ten Cate <ttencate@gmail.com>
2019-12-19 09:34:21 +0100

    Optimize 19a by tracing beam top and bottom

--- src/bin/19.rs
+++ src/bin/19.rs
@@ -1,18 +1,65 @@
 use aoc::intcode::*;
+use std::ops::Range;
+
+#[derive(Debug)]
+struct BeamTracer {
+    program: Program,
+    last_slice: BeamSlice,
+}
+
+#[derive(Debug, Clone)]
+struct BeamSlice {
+    x: Number,
+    ys: Range<Number>,
+}
+
+impl BeamTracer {
+    fn new(program: Program) -> Self {
+        BeamTracer { program, last_slice: BeamSlice { x: -1, ys: 0..0 } }
+    }
+}
+
+impl Iterator for BeamTracer {
+    type Item = BeamSlice;
+    fn next(&mut self) -> Option<BeamSlice> {
+        let slice = &mut self.last_slice;
+        slice.x += 1;
+        if slice.x < 12 {
+            slice.ys.start = 0;
+            while slice.ys.start < 12 &&
+                self.program.clone().run_with_io(vec![slice.x, slice.ys.start]).output[0] == 0
+            {
+                slice.ys.start += 1;
+            }
+            slice.ys.end = slice.ys.start;
+            while slice.ys.end < 12 &&
+                self.program.clone().run_with_io(vec![slice.x, slice.ys.end]).output[0] == 1
+            {
+                slice.ys.end += 1;
+            }
+        } else {
+            while self.program.clone().run_with_io(vec![slice.x, slice.ys.start]).output[0] == 0 {
+                slice.ys.start += 1;
+            }
+            while self.program.clone().run_with_io(vec![slice.x, slice.ys.end]).output[0] == 1 {
+                slice.ys.end += 1;
+            }
+        }
+        Some(slice.clone())
+    }
+}
 
 fn part1(input: &str) -> usize {
     let program = Program::parse(input);
+    // for y in 0..80 {
+    //     for x in 0..80 {
+    //         print!("{}", if program.clone().run_with_io(vec![x, y]).output[0] == 1 { '#' } else { '.' });
+    //     }
+    //     println!();
+    // }
     let mut count = 0;
-    for y in 0..50 {
-        for x in 0..50 {
-            let (attracted, _) = program.clone()
-                .run().give_input(x)
-                .run().give_input(y)
-                .run().take_output();
-            if attracted != 0 {
-                count += 1;
-            }
-        }
+    for slice in BeamTracer::new(program).take(50) {
+        count += (slice.ys.end - slice.ys.start) as usize;
     }
     count
 }

cc49a1f0b34cf7ac149b4c2417b91b60d82f788e
Thomas ten Cate <ttencate@gmail.com>
2019-12-19 09:11:05 +0100

    Solve 19a naively

--- src/bin/19.rs
+++ src/bin/19.rs
@@ -1,5 +1,20 @@
-fn part1(_input: &str) -> String {
-    "TODO".to_string()
+use aoc::intcode::*;
+
+fn part1(input: &str) -> usize {
+    let program = Program::parse(input);
+    let mut count = 0;
+    for y in 0..50 {
+        for x in 0..50 {
+            let (attracted, _) = program.clone()
+                .run().give_input(x)
+                .run().give_input(y)
+                .run().take_output();
+            if attracted != 0 {
+                count += 1;
+            }
+        }
+    }
+    count
 }
 
 fn part2(_input: &str) -> String {
@@ -12,5 +27,5 @@ fn main() {
 
 #[test]
 fn test_answers() {
-    // aoc::test(part1, "TODO".to_string(), part2, "TODO".to_string());
+    // aoc::test(part1, 131, part2, "TODO".to_string());
 }
